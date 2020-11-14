e2e36cd99e80cfe4bdf94cd158ef7f52dd7bd3e5
wellytansil <we2y.tansil@gmail.com>
2020-11-14 16:45:10 +0700

    ready to deploy

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,12 +1,12 @@
 const jwt = require("jsonwebtoken")
 
 function generateToken(payload) {
-    const token = jwt.sign(payload, 'hehe')
+    const token = jwt.sign(payload, process.env.SECRET)
     return token
 }
 
 function decodingToken(token) {
-    const decoded = jwt.verify(token, 'hehe')
+    const decoded = jwt.verify(token, process.env.SECRET)
     return decoded
 }
 
